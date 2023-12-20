fa8ecbfd7624cf0450f629167150d19228d1a207
Pedro Costa <costapm96@gmail.com>
2023-12-20 16:28:02 +0000

    Fiz tudo

--- MTE1/src/main/java/org/example/Figure.java
+++ MTE1/src/main/java/org/example/Figure.java
@@ -7,4 +7,5 @@ public abstract class Figure {
     public Figure (String color){
         this.color=color;
     }
+    public abstract double area();
 }
--- MTE1/src/main/java/org/example/Rectangle.java
+++ MTE1/src/main/java/org/example/Rectangle.java
@@ -6,9 +6,28 @@ public class Rectangle extends Figure {
 
     private static int global = 14;
 
-    public Rectangle(Point x, Point y){
+    public Rectangle(Point x, Point y,String color){
+        super(color);
         this.x = x;
         this.y = y;
     }
 
+    public Point getX() {
+        return x;
+    }
+
+    public void setX(Point x) {
+        this.x = x;
+    }
+
+    public Point getY() {
+        return y;
+    }
+
+    public void setY(Point y) {
+        this.y = y;
+    }
+    public double area(){
+        return Math.abs(x.getCoordinate1()-y.getCoordinate1())*Math.abs(x.getCoordinate2()-y.getCoordinate2());
+    }
 }
