747d718a5356c6049a70e59703ff2ea9089ca236
Rafael <rafa.figueiredo48@gmail.com>
2023-12-20 16:14:11 +0000

    Added Figure Class

---
+++ MTE1/src/main/java/org/example/Figure.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public abstract class Figure {
+
+    private String color;
+
+    public Figure (String color){
+        this.color=color;
+    }
+}
--- MTE1/src/main/java/org/example/Rectangle.java
+++ MTE1/src/main/java/org/example/Rectangle.java
@@ -1,10 +1,12 @@
 package org.example;
 
-public class Rectangle {
+public class Rectangle extends Figure {
     private Point x;
     private Point y;
+    private static int global = 14;
     public Rectangle(Point x, Point y){
         this.x = x;
         this.y = y;
     }
+
 }
