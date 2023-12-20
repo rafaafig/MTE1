6848b58f4e1faa6585c582607bcffcb8488b4799
Rafael <rafa.figueiredo48@gmail.com>
2023-12-20 16:25:43 +0000

    Figure class complete

--- MTE1/src/main/java/org/example/Figure.java
+++ MTE1/src/main/java/org/example/Figure.java
@@ -7,4 +7,19 @@ public abstract class Figure {
     public Figure (String color){
         this.color=color;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Figure{" +
+                "color='" + color + '\'' +
+                '}';
+    }
 }
