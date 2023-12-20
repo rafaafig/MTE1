aaa1c9110e78354615f17495f282ba74f67e9d85
Pedro Costa <costapm96@gmail.com>
2023-12-20 16:13:53 +0000

    Changed global value

--- MTE1/src/main/java/org/example/Rectangle.java
+++ MTE1/src/main/java/org/example/Rectangle.java
@@ -3,6 +3,7 @@ package org.example;
 public class Rectangle {
     private Point x;
     private Point y;
+    private static int global = 15;
     public Rectangle(Point x, Point y){
         this.x = x;
         this.y = y;
