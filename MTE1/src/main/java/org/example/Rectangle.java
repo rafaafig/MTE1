b1578010653714d355a85bdbfb4fc5be69715a2a
Pedro Costa <costapm96@gmail.com>
2023-12-20 16:09:32 +0000

    Added Rectangle class

---
+++ MTE1/src/main/java/org/example/Rectangle.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Rectangle {
+    private Point x;
+    private Point y;
+    public Rectangle(Point x, Point y){
+        this.x = x;
+        this.y = y;
+    }
+}
